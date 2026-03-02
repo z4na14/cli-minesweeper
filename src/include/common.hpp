468de1f09d4bbe417a2df4a13ebabaf2420302f9
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2026-03-03 00:58:10 +0100

    refactor

--- src/include/common.hpp
+++ src/include/common.hpp
@@ -10,9 +10,9 @@ typedef enum {
 } Layer;
 
 namespace common {
-    constexpr std::string colorReset {"\x1b[0m"};
+    constexpr std::string colorReset{"\x1b[0m"};
 
-    template <Layer L>
+    template<Layer L>
     struct color {
         uint8_t r, g, b;
         bool transparent;
@@ -21,38 +21,43 @@ namespace common {
 
         color(const uint8_t r,
               const uint8_t g,
-              const uint8_t b): r(r), g(g), b(b), transparent(false) {}
-        color(): r(255), g(255), b(255), transparent(true) {}
+              const uint8_t b) : r(r), g(g), b(b), transparent(false) {
+        }
+
+        color() : r(255), g(255), b(255), transparent(true) {
+        }
 
         // ---
 
-        friend std::ostream& operator<< (std::ostream& os, color const& c) {
+        friend std::ostream &operator<<(std::ostream &os, color const &c) {
             // If we are printing a transparent color, just return nothing
             if (c.transparent) return os;
 
             return os << "\x1b[" << L << ";2;"
-                      << static_cast<int>(c.r) << ";"
-                      << static_cast<int>(c.g) << ";"
-                      << static_cast<int>(c.b) << "m";
+                   << static_cast<int>(c.r) << ";"
+                   << static_cast<int>(c.g) << ";"
+                   << static_cast<int>(c.b) << "m";
         }
     };
 
     struct cell {
-        std::string val {};
-        color<Foreground> fg {};
-        color<Background> bg {};
+        std::string val{};
+        color<Foreground> fg{};
+        color<Background> bg{};
 
         // ---
 
         cell(std::string val,
-             color<Foreground> const& fg,
-             color<Background> const& bg): val(std::move(val)), fg(fg), bg(bg) {}
-        explicit cell(std::string val):
-            val(std::move(val)), fg({255, 255, 255}), bg({} /*Transparent bg*/) {}
+             color<Foreground> const &fg,
+             color<Background> const &bg) : val(std::move(val)), fg(fg), bg(bg) {
+        }
+
+        explicit cell(std::string val) : val(std::move(val)), fg({255, 255, 255}), bg({} /*Transparent bg*/) {
+        }
 
         // ---
 
-        friend std::ostream& operator<< (std::ostream& os, cell const& c) {
+        friend std::ostream &operator<<(std::ostream &os, cell const &c) {
             return os << c.fg << c.bg << c.val << colorReset;
         }
     };
--- src/include/render.hpp
+++ src/include/render.hpp
@@ -5,47 +5,68 @@
 #include "common.hpp"
 
 namespace render {
+    // 32 Hz
+    constexpr uint8_t FRAMERATE_PERIOD{1 / 64 * 100'000};
+
     struct controls {
-        static constexpr std::string_view clear_screen = "\x1b[2J\x1b[H";
-        static constexpr std::string_view clear_line   = "\x1b[2K";
-        static constexpr std::string_view clear_buffer = "\x1b[3J\x1b[H";
-        static constexpr std::string_view show_cursor  = "\x1b[?25h";
-        static constexpr std::string_view hide_cursor  = "\x1b[?25l";
+        static constexpr std::string_view CLEAR_SCREEN = "\x1b[2J\x1b[H";
+        static constexpr std::string_view CLEAR_LINE = "\x1b[2K";
+        static constexpr std::string_view CLEAR_BUFFER = "\x1b[3J\x1b[H";
+        static constexpr std::string_view SHOW_CURSOR = "\x1b[?25h";
+        static constexpr std::string_view HIDE_CURSOR = "\x1b[?25l";
+        static constexpr std::string_view ENABLE_MOUSE = "\033[?1000h";
+        static constexpr std::string_view ENABLE_SGR1006 = "\033[?1006h";
+        static constexpr std::string_view DISABLE_MOUSE = "\033[?1000l";
+        static constexpr std::string_view DISABLE_SGR1006 = "\033[?1006l";
     };
 
     struct interfaceChars {
         // Corners
-        static constexpr std::string_view lt_corner {"┌"};
-        static constexpr std::string_view lb_corner {"└"};
-        static constexpr std::string_view rt_corner {"┐"};
-        static constexpr std::string_view rb_corner {"┘"};
+        static constexpr std::string_view LT_CORNER{"┌"};
+        static constexpr std::string_view LB_CORNER{"└"};
+        static constexpr std::string_view RT_CORNER{"┐"};
+        static constexpr std::string_view RB_CORNER{"┘"};
 
         // Bars
-        static constexpr std::string_view h_bar {"─"};
-        static constexpr std::string_view v_bar {"│"};
+        static constexpr std::string_view H_BAR{"─"};
+        static constexpr std::string_view V_BAR{"│"};
 
         // T-Junctions
-        static constexpr std::string_view t_top    {"┬"};
-        static constexpr std::string_view t_bottom {"┴"};
-        static constexpr std::string_view t_left   {"├"};
-        static constexpr std::string_view t_right  {"┤"};
-        static constexpr std::string_view cross    {"┼"};
+        static constexpr std::string_view T_TOP{"┬"};
+        static constexpr std::string_view T_BOTTOM{"┴"};
+        static constexpr std::string_view T_LEFT{"├"};
+        static constexpr std::string_view T_RIGHT{"┤"};
+        static constexpr std::string_view CROSS{"┼"};
     };
 
     struct colors {
-        static constexpr std::array<uint8_t, 3> white  {255,255,255};
-        static constexpr std::array<uint8_t, 3> red    {255,0  ,0  };
-        static constexpr std::array<uint8_t, 3> green  {0  ,255,0  };
-        static constexpr std::array<uint8_t, 3> blue   {0  ,0  ,255};
+        static constexpr std::array<uint8_t, 3> WHITE{255, 255, 255};
+        static constexpr std::array<uint8_t, 3> RED{255, 0, 0};
+        static constexpr std::array<uint8_t, 3> GREEN{0, 255, 0};
+        static constexpr std::array<uint8_t, 3> BLUE{0, 0, 255};
     };
 
     struct termSize_t {
         int rows;
         int cols;
+
+        friend bool operator==(const termSize_t &lhs, const termSize_t &rhs) {
+            return lhs.rows == rhs.rows && lhs.cols == rhs.cols;
+        }
     };
 
-    void init_runtime();
+    void init_bootstrap();
+
+    void loop_runtime();
+
+    void cleanup_bootstrap();
+
     termSize_t get_termSize();
+
+    void setRawMode(bool enable);
+
+    // Threading function to get input
+    void get_term_input_(std::string &buff);
 }
 
 #endif //RENDER_HPP
